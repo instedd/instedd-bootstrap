95a79879dcd107adb58b40d3e2c330f175efc355
Brian J. Cardiff <bcardiff@manas.com.ar>
2014-03-19 06:43:42 -0300

    refactor cleanup and helpers. WIP

--- app/helpers/instedd-bootstrap/application_helper.rb
+++ app/helpers/instedd-bootstrap/application_helper.rb
@@ -17,5 +17,51 @@
 
 module InsteddBootstrap
   module ApplicationHelper
+    def top_nav_bar
+      content_for :top_nav_bar do
+        yield
+      end
+    end
+
+    def top_nav_bar_item(text, path, active = false)
+      active ||= request.path == path
+      haml_tag :li, class: "#{'active' if active}" do
+        haml_concat link_to(text, path)
+      end
+    end
+
+    def breadcrumb
+      haml_tag :ul, class: "breadcrumb" do
+        @breadcrumb_items = []
+        yield
+
+        @breadcrumb_items.each_with_index do |item, index|
+          if index == @breadcrumb_items.length - 1
+            _breadcrumb_last_item item[:text]
+          else
+            _breadcrumb_item item[:text], item[:path]
+          end
+        end
+      end
+    end
+
+    def breadcrumb_item(text, path = nil)
+      @breadcrumb_items << { text: text, path: path }
+    end
+
+    def _breadcrumb_item(text, path)
+      haml_tag :li do
+        haml_concat link_to(text, path)
+      end
+    end
+
+    def _breadcrumb_last_item(text)
+      haml_tag :li, class: 'active' do
+        haml_tag :span
+        haml_concat text
+      end
+    end
   end
 end
+
+ActionController::Base.send(:helper, InsteddBootstrap::ApplicationHelper)
--- lib/instedd-bootstrap.rb
+++ lib/instedd-bootstrap.rb
@@ -18,5 +18,7 @@
 require "instedd-bootstrap/engine"
 require "bootstrap-sass"
 
+require_relative "../app/helpers/instedd-bootstrap/application_helper"
+
 module InsteddBootstrap
 end
